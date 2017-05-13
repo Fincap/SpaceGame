6a5cd4b5318cbbac1a4dbb358130f48480b3abe2
Matt Archer <finnkerman@gmail.com>
2017-05-13 13:44:13 +1000

    Added jump ranges
    - Fixed stars being able to overlap eachother
    - Now highlights how far each empire can jump on selecting star (Needs
    to be changed to showing how far the player's empire can jump on each
    star)

--- src/SpaceGame.py
+++ src/SpaceGame.py
@@ -5,15 +5,19 @@ GAMEH = 936
 UIX =  1248
 FPS = 60
 
-STARCOUNT = 300
+STARCOUNT = 150
 
 os.environ['SDL_VIDEO_CENTERED'] = '1'
 pygame.init()
 screen = pygame.display.set_mode((GAMEW, GAMEH))
-pygame.display.set_caption("Space Game v0.1")
+pygame.display.set_caption("Space Game v0.1 (Alpha)")
 fpsClock = pygame.time.Clock()
 
 class Cursor(object):
+
+	def __init__(self):
+		self.selected = None
+		self.texture = pygame.image.load(os.path.join('img', 'misc_icons', 'cursor.png')).convert()
 
 	def pos(self):
 		return pygame.mouse.get_pos()
@@ -24,15 +28,21 @@ class Cursor(object):
 	def y(self):
 		return self.pos()[1]
 
+	def render(self):
+		if self.selected != None:
+			screen.blit(self.texture, (self.selected.x - 5, self.selected.y - 5))
+			if self.selected.owner != None:
+				pygame.draw.circle(screen, (133, 133, 133), (self.selected.x, self.selected.y), self.selected.owner.jumpRange, 2) #Draws the jump range of ships on planet
+
 class Star(object):
 
 	stars = []
 
 	def __init__(self, name, x, y):
 		self.name = name
-		self.x = x
-		self.y = y
-		self.rect = pygame.Rect(self.x, self.y, 2, 2)
+		self.x = x + 5
+		self.y = y + 5
+		self.rect = pygame.Rect(self.x - 5, self.y - 5, 10, 10)
 		self.owner = None
 
 	def render(self):
@@ -40,7 +50,7 @@ class Star(object):
 			self.color = self.owner.color
 		else:
 			self.color = (255, 255, 255)
-		pygame.draw.rect(screen, self.color, self.rect)
+		pygame.draw.circle(screen, self.color, (self.x, self.y), 5)
 
 	def render_stars():
 		for star in Star.stars:
@@ -53,7 +63,9 @@ class Empire(object):
 	def __init__(self, name, color):
 		self.name = name
 		self.color = color
+		self.player = False
 		self.capital = self.generate_capital()
+		self.jumpRange = 100
 
 	def generate_capital(self):
 		isUnique = False
@@ -69,11 +81,13 @@ def build_universe():
 	alreadyOccupied = []
 	count = 0
 	while count < STARCOUNT:
-		randx = random.randint(5, UIX)
-		randy = random.randint(5, GAMEH)
+		randx = random.randint(10, UIX - 10)
+		randy = random.randint(10, GAMEH - 10)
 		if (randx, randy) not in alreadyOccupied:
+			for i in range(randx - 10, randx + 10):
+				for j in range(randy - 10, randy + 10):
+					alreadyOccupied.append((i, j)) #Adds every coordinate in a 10 radius around the star to already occupied, preventing stars from spawning too close to eachother
 			Star.stars.append(Star("Star", randx, randy))
-			alreadyOccupied.append((randx, randy))
 			count += 1
 
 def handle_input():
@@ -85,12 +99,22 @@ def handle_input():
 			if event.key == pygame.K_ESCAPE:
 				sys.exit()
 
+			if event.key == pygame.K_BACKQUOTE:
+				Cursor.selected = None
+
+		if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+			for star in Star.stars:
+				if star.rect.collidepoint(Cursor.pos()):
+					Cursor.selected = star
+
 #Initializiation
 Cursor = Cursor()
 build_universe()
 
 #Define empires
-Man = Empire("Empire of Man", (0, 0, 255))
+Republic = Empire("Galactic Republic", (0, 0, 255))
+Jedi = Empire("The Jedi Order", (0, 255, 0))
+Sith = Empire("The Sith Lords", (255, 0, 0))
 
 #Game loop
 while True:
@@ -99,6 +123,9 @@ while True:
 
 	screen.fill((0,0,0))
 	Star.render_stars()
+	Cursor.render()
+
+	pygame.draw.rect(screen, (0,0,0), (UIX, 0, 1000, GAMEH))
 
 	pygame.display.update()
 	fpsClock.tick(FPS)
